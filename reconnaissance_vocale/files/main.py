cead1f2b19af7061e3c625c878d8558a2b0e7f3a
tinoraholison <thierry.bercytino@gmail.com>
2024-11-08 16:16:10 +0300

    Modification de tout les fichiers

--- reconnaissance_vocale/files/main.py
+++
@@ -1,7 +0,0 @@
-from generate_data_json import generate_data_json
-from load_and_prepare_datasets import load_and_prepare_dataset
-from train_model import train_model
-from test_model import test_model
-
-# Génération du fichier JSON des données
-generate_data_json("reconnaissance_vocale/files/data", "data.json")

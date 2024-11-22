c42d279f15965b6db3cef9aab52ec60d486d38c7
tinoraholison <thierry.bercytino@gmail.com>
2024-11-22 11:32:29 +0300

    Modification du fichier main.py

--- reconnaissance_vocale/files/main.py
+++ reconnaissance_vocale/files/main.py
@@ -6,20 +6,14 @@ def main():
     data_dir = "reconnaissance_vocale/files/data"
     json_file = "data.json"
     
-    # Charger et préparer les données
-    print("Chargement et préparation des données...")
+    # Chargement et préparation des données
     dataset, labels = load_and_prepare_data(data_dir, json_file)
-    print("Données chargées et préparées.")
     
-    # Entraîner le modèle
-    print("Entraînement du modèle...")
-    train_model(dataset, labels)
-    print("Modèle entraîné.")
+    # Entraînement du modèle
+    train_model(dataset)
     
-    # Tester le modèle
-    print("Test du modèle...")
+    # Teste du modèle
     test_model(dataset, labels)
-    print("Modèle testé.")
 
 if __name__ == "__main__":
     main()
